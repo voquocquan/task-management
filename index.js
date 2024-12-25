811e19b3cfcce4a1f1dc6c87c399360700abde7e
Võ Quốc Quân <vo.qquan@gmail.com>
2024-12-25 21:26:14 +0700

    lấy ra chi tiết công việc

--- index.js
+++ index.js
@@ -14,7 +14,16 @@ app.get("/tasks", async (req, res) => {
   const tasks = await Task.find({
     deleted: false
   })
-  console.log(tasks);
+  
+  res.json(tasks)
+});
+
+app.get("/tasks/detail/:id", async (req, res) => {
+  const id = req.params.id
+  const tasks = await Task.findOne({
+    _id:id,
+    deleted: false
+  })
   
   res.json(tasks)
 })
