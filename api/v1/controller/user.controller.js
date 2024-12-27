132a1be4cfc3cef3aeac1b3654b37da680c43ab1
Võ Quốc Quân <vo.qquan@gmail.com>
2024-12-27 13:16:07 +0700

    tính năng đăng nhập

--- api/v1/controller/user.controller.js
+++ api/v1/controller/user.controller.js
@@ -35,3 +35,39 @@ module.exports.register = async (req, res) => {
     })
   }
 };
+
+// [GET] /api/v1/users/login
+module.exports.login = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = await User.findOne({
+    email: email,
+    deleted: false
+  });
+
+  if(!user) {
+    res.json({
+      code: 400,
+      message: "Email không tồn tại!"
+    });
+    return;
+  };
+
+  if(md5(password) !== user.password) {
+    res.json({
+      code: 400,
+      message: "Sai mật khẩu!"
+    });
+    return;
+  };
+
+  const token = user.token;
+    res.cookie("token", token);
+
+    res.json({
+      code: 200,
+      message: "Đăng nhập thành công",
+      token: token
+    })
+};
--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -6,5 +6,7 @@ const controller = require("../controller/user.controller")
 
 router.post("/register", controller.register);
 
+router.post("/login", controller.login);
+
 
 module.exports = router;
