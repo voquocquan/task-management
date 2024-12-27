9beefbe20a923ceb38b45254a160a952b0a3c049
Võ Quốc Quân <vo.qquan@gmail.com>
2024-12-27 16:34:12 +0700

    thông tin cá nhân

--- api/v1/controller/user.controller.js
+++ api/v1/controller/user.controller.js
@@ -181,3 +181,19 @@ module.exports.resetPassword = async (req, res) => {
   
 };
 
+// [GET] /api/v1/users/detail
+module.exports.detail = async (req, res) => {
+  const token = req.cookies.token;
+
+  const user = await User.findOne({
+    token: token,
+    deleted: false
+  }).select(" -password -token");
+
+  res.json({
+    code: 200,
+    message: "Thành công",
+    info: user
+  });
+};
+
--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -14,5 +14,7 @@ router.post("/password/otp", controller.otpPassword);
 
 router.post("/password/reset", controller.resetPassword);
 
+router.get("/detail", controller.detail);
+
 
 module.exports = router;
