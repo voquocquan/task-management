1835f4b389c6eca535356f42048abc29b633a23a
Võ Quốc Quân <vo.qquan@gmail.com>
2024-12-27 15:18:33 +0700

    màn hình nhập email lấy mã otp

--- api/v1/controller/user.controller.js
+++ api/v1/controller/user.controller.js
@@ -1,7 +1,10 @@
 const md5 = require("md5");
-const User = require("../models/user.model")
+const User = require("../models/user.model");
+const ForgotPassword = require("../models/forgot-password.model");
 
-// [GET] /api/v1/users
+const generateHelper = require("../../../helpers/generate");
+const sendMailHelper = require("../../../helpers/sendMail");
+// [POST] /api/v1/users
 module.exports.register = async (req, res) => {
   req.body.password = md5(req.body.password);
 
@@ -36,7 +39,7 @@ module.exports.register = async (req, res) => {
   }
 };
 
-// [GET] /api/v1/users/login
+// [POST] /api/v1/users/login
 module.exports.login = async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
@@ -71,3 +74,47 @@ module.exports.login = async (req, res) => {
       token: token
     })
 };
+
+// [POST] /api/v1/users/password/forgot
+module.exports.forgotPassword = async (req, res) => {
+  const email = req.body.email;
+
+  const user = await User.findOne({
+    email: email,
+    deleted: false
+  });
+
+  if(!user) {
+    res.json({
+      code: 400,
+      message: "Email không tồn tại!"
+    });
+    return;
+  };
+
+  const otp = generateHelper.generateRandomNumber(4);
+
+  const timeExpire = 5;
+
+  //Lưu data vào database
+  const objectForgotPassword = {
+    email: email,
+    otp: otp,
+    expireAt: Date.now() + timeExpire*60,
+  };
+  const forgotPassword = new ForgotPassword(objectForgotPassword);
+  await forgotPassword.save();
+
+  // Gửi OTP qua email user
+  const subject = "Mã OTP xác minh lấy lại mật khẩu"
+  const html = `Mã xác minh của bạn là <b>${otp}</b>. Sử dụng trong ${timeExpire} phút.
+    Vui lòng không cung cấp hoặc chia sẻ cho người khác. `
+
+  sendMailHelper.sendMail(email, subject, html); 
+  
+  res.json({
+    code: 200,
+    message: "Đã gửi OTP qua email!"
+  });
+};
+
--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -8,5 +8,7 @@ router.post("/register", controller.register);
 
 router.post("/login", controller.login);
 
+router.post("/password/forgot", controller.forgotPassword);
+
 
 module.exports = router;
